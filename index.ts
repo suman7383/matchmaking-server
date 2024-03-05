a84d5d1628a6d68ca4f02edb6bfbf5779f0b0480
Suman Mukherjee <sumanmukherjee2001@gmail.com>
2024-03-05 19:29:17 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import "dotenv/config";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import Queue from "./services/Queue";
+
+const PORT = process.env.PORT || 8000;
+
+const queue = new Queue();
+
+async function main() {
+  const app = express();
+  const httpServer = createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connect", async (socket) => {
+    console.log(`A new Socket joined the waiting queue : ${socket.id}`);
+    //put this socket into the queue
+    queue.push(socket);
+
+    socket.on("disconnect", () => {
+      console.log(`Scoket ${socket.id} disconnected!`);
+    });
+  });
+
+  httpServer.listen(PORT);
+  console.log(`Server running on port ${PORT}`);
+}
+
+main();
